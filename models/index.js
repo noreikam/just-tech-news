20f4140351e72507fa3f50c3c68b3ce35ef5bac7
noreikam <noreikam1@gmail.com>
2022-04-23 23:24:38 -0400

    added voting

--- models/index.js
+++ models/index.js
@@ -1,13 +1,40 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
-// create associations
+// create user-post associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 Post.belongsTo(User, {
     foreignKey: 'user_id'
+});
+
+// create vote associations
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+})
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
 })
 
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
