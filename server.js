673e53d3ee29f829c0799d96e94dbb195cc8d493
noreikam <noreikam1@gmail.com>
2022-04-25 14:34:30 -0400

    established user authentication and session

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const sess = {
+    secret: process.env.SESS_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
@@ -18,7 +32,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // turn on routes
-app.use(routes);
+app.use(require('./controllers'));
+
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
